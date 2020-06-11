f116068a4ab2f358e018501c6c10537bf4901374
Rajath <rajathedgefinity@gmail.com>
2020-06-11 16:35:35 +0530

    Crud Operation Created a Notes with id 1

---
+++ src/app/api/notes.py
@@ -0,0 +1,19 @@
+from app.api import crud
+from app.api.schema import NoteSchema, NoteDB
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+
+@router.post("/", response_model=NoteDB, status_code=201)
+async def create_note(payload: NoteSchema):
+    note_id = await crud.post(payload)
+
+    response_object = {
+        "id": note_id,
+        "title": payload.title,
+        "description": payload.description,
+    }
+
+    return response_object
